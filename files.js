95b96b22df28c2b9ba45a5ac0ace7a0e2115cade
dpasiak <57316056+DP-00@users.noreply.github.com>
2023-07-20 22:37:03 +0200

    #5 Map: loading and displaying layers (#48)

--- files.js
+++ files.js
@@ -1,100 +1,132 @@
 //  to git ignore
-const ACCESS_TOKEN = ""; // token expires after around one day
-
+const ACCESS_TOKEN = "sl.BimHWlZLfzKvIbLbkQG5MO2I4dovRfhxwX87xKRlA3l2J2Q33TyzH8lr3Om61ZS1VjniblHzr49E9jf_7h71gYhYL3GY9c7IPJzYX_5c7k_9vJc1UtFsC04cDVBPSK0UdGl62XkCoGZ0Xm5jPxDJ"; // token expires after around one day
+const dataPath = "data/"
 let layerList;
 let dropboxButton = document.getElementById("dropbox-button");
 var data;
-export async function loadFiles(option){
 
-	switch(option){
-		case "local":
-			let dataLocal =  await getConfig("data/conf.json");
-			return dataLocal;
-			
-		case "DropboxToken":
-			let dbx = new Dropbox.Dropbox({ accessToken: ACCESS_TOKEN });
-			let dataDropboxToken = await dbx.usersGetCurrentAccount().then(function(response) {
-					console.log("user", response);
-					return dbx.filesGetTemporaryLink({
-					path: '/conf.json'
-				})
-			}).then((response) => {
-				console.log(response.result.link);
-				return getConfig(response.result.link);
-			}).catch(function(error) {
-				console.error(error);
-			});
-			
-			return dataDropboxToken;
+// DROPBOX FUNCTIONS
+const REDIRECT_URI = 'http://localhost:8000/';
+const dbxAuth = new Dropbox.DropboxAuth({
+	clientId: '2h95rnvy2dubcsw',
+});
 
-		case "Dropbox":
-			var dbxAll = '';
-			const REDIRECT_URI = 'http://localhost:8000/';
-			var dbxAuth = new Dropbox.DropboxAuth({
-				clientId: '2h95rnvy2dubcsw',
-			});
-
-			function getCodeFromUrl() {
-				return utils.parseQueryString(window.location.search).code;
-			}
-			
-			function hasRedirectedFromAuth() {
-				return !!getCodeFromUrl();
-			}
-			
-			function showPageSection(elementId) {
-				document.getElementById(elementId).style.display = 'block';
-			}
-			
-			function doAuth() {
-				dbxAuth.getAuthenticationUrl(REDIRECT_URI, undefined, 'code', 'offline', undefined, undefined, true)
-					.then(authUrl => {
-						window.sessionStorage.clear();
-						window.sessionStorage.setItem("codeVerifier", dbxAuth.codeVerifier);
-						window.location.href = authUrl;
-					})
-					.catch((error) => console.error(error));
-			};
-			let data;
-			if (hasRedirectedFromAuth()) {
-				showPageSection('authed-section');
-				dbxAuth.setCodeVerifier(window.sessionStorage.getItem('codeVerifier'));
-				data = await dbxAuth.getAccessTokenFromCode(REDIRECT_URI, getCodeFromUrl())
-					.then((response) => {
-						dbxAuth.setAccessToken(response.result.access_token);
-						var dbx = new Dropbox.Dropbox({
-							auth: dbxAuth
-						});
-						dbxAll = dbx;
-						return dbx.filesGetTemporaryLink({
-							path: '/conf.json'
-						})
-					})
-					.then((response) => {	
-						console.log(response.result.link)					
-						return getConfig(response.result.link)
-					})
-					.catch((error) => {
-						console.error(error)
-					});
-			} else {
-				showPageSection('pre-auth-section');
-			}
-			
-			
-			dropboxButton.addEventListener("click", function(){doAuth()})
-			console.log(data);
-			return data;
-	}	
+function getCodeFromUrl() {
+	return utils.parseQueryString(window.location.search).code;
 }
 
-async function getConfig(file){
+function hasRedirectedFromAuth() {
+	return !!getCodeFromUrl();
+}
 
-	const res = await fetch(file);
-	const configJSON = await res.json();
-	console.log(configJSON);
+function showPageSection(elementId) {
+	document.getElementById(elementId).style.display = 'block';
+}
 
-	return configJSON;
+function doAuth() {
+	dbxAuth.getAuthenticationUrl(REDIRECT_URI, undefined, 'code', 'offline', undefined, undefined, true)
+		.then(authUrl => {
+			window.sessionStorage.clear();
+			window.sessionStorage.setItem("codeVerifier", dbxAuth.codeVerifier);
+			window.location.href = authUrl;
+		})
+		.catch((error) => console.error(error));
+};
+
+export async function loadDropbox(){
+	dropboxButton.addEventListener("click", function(){doAuth()})
+	let dbx;
+	if (hasRedirectedFromAuth()) {
+		showPageSection('authed-section');
+		dbxAuth.setCodeVerifier(window.sessionStorage.getItem('codeVerifier'));
+		data = await dbxAuth.getAccessTokenFromCode(REDIRECT_URI, getCodeFromUrl())
+			.then((response) => {
+				dbxAuth.setAccessToken(response.result.access_token);
+				dbx = new Dropbox.Dropbox({
+					auth: dbxAuth
+				});
+				return dbx;
+			})
+			.catch((error) => {
+				console.error(error)
+			});
+	} else {
+		showPageSection('pre-auth-section');
+	}
+
+	return dbx;
+}
+
+export async function loadDropboxToken(){
+	let dbx = new Dropbox.Dropbox({ accessToken: ACCESS_TOKEN });
+	await dbx.usersGetCurrentAccount().then(function(response) {
+		console.log("user", response);
+	})
+	return dbx;
+}
+
+// empty string = local files, otherwise dropbox object is passed
+export async function loadFiles(fileSource){
+
+	if(!fileSource){
+		let dataLocal =  await fetchFile("data/conf.json");
+		return dataLocal;
+	}else{
+		let dataDropbox = await fetchDropboxFile(fileSource, '/conf.json')
+		return dataDropbox;
+	}
+}
+
+
+async function fetchFile(link){
+	let result = await fetch(link);
+	// console.log(result);	
+	let layer = await result.json();
+	console.log(layer);		
+	return layer;
+}
+
+async function fetchDropboxLink(dbx, link){
+	let dropboxLink = await dbx.filesGetTemporaryLink({path: link}).then(function(response) {
+		console.log(response.result.link);	
+		return 	response.result.link;
+	})
+	return dropboxLink;
+}
+
+async function fetchDropboxFile(dbx, link){
+	let dropboxLink = await fetchDropboxLink(dbx, link)
+	let data = await fetchFile(dropboxLink);
+
+	return data;
+}
+
+// layers = file.layers
+export async function listLayers(map, layers, dataSource){
+	console.log(layers);
+	for (let i=0; i< layers.length; i++){
+		let path = layers[i].name + '.geojson';
+		console.log(path);
+		let layerFile;
+		if(!dataSource){
+			path = dataPath + layers[i].name + '.geojson';
+		}else{
+			// console.log(dataSource, path);
+			path = await fetchDropboxLink(dataSource, '/'+path);
+		}
+
+		let source = new ol.source.Vector({	
+			url: path,
+			format: new ol.format.GeoJSON()
+		});
+
+		map.addLayer(new ol.layer.Vector(
+			{	name: layers[0]["name"],
+				source: source,
+				zIndex: 5,
+			}
+		));	
+	}
 }
 
 
@@ -102,44 +134,7 @@ async function getConfig(file){
 	// dbx.filesUpload({path: '/' + file.name, contents: file}).then(function(response) {
     //     console.log(response);
     // })
-
-	// dbx.sharingGetSharedLinkFile({url: SHARED_LINK}).then(function(data) {
-		// var downloadUrl = URL.createObjectURL(data.fileBlob);
-		// var downloadButton = document.createElement('a');
-		// downloadButton.setAttribute('href', downloadUrl);
-		// downloadButton.setAttribute('download', data.name);
-		// downloadButton.setAttribute('class', 'button');
-		// downloadButton.innerText = 'Download: ' + data.name;
-		// document.getElementById('results').appendChild(downloadButton);
-	// })
     				
 	// return dbx.filesListFolder({
 	// 	path: ''
 	// });
-    
-
-    // var file = 	''
-    // dbxAll.filesGetTemporaryLink({path: path})
-    // .then(function(response) {
-    //     file = response.result.link;	
-    //     console.log(file);	
-    //     // var res1 = await fetch(file);
-    //     // console.log(res1);	
-    //     // var configJSON1 = await res1.json();
-    //     // console.log(configJSON1);		
-
-    //     // layers[i] = await response.json(); // creating arrays with geojson objects
-    //             // var geojsonUrl = URL.createObjectURL(data.fileBlob);
-
-    //     // creating layers
-    //     map.addLayer(new ol.layer.Vector(
-    //         {	
-    //             name: "test data",
-    //             source: new ol.source.Vector({	
-    //                 url: data,
-    //                 format: new ol.format.GeoJSON()
-    //             }),
-    //             zIndex: 5,
-    //     }));
-            
-    // })
