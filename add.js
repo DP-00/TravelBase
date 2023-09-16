040f774676d3a878825e219fef34502b1832347d
DP-00 <pasiak.dagmara@gmail.com>
2023-09-16 16:52:55 +0200

    Map: editing or deleting single entries by pop-ups Fixes #17

--- add.js
+++ add.js
@@ -65,12 +65,12 @@ function listPropertiesByFeatures(features){
 	return propKeys;
 }
 
-function listPropertiesByLayer(config){
+export function listPropertiesByLayer(layerName, config){
 	// let layerKeys =[];
 	const layerKeys = new Map();
 
 	for(var i=0; i<config["layers"].length; i++){
-		if(selectAdd.value === config["layers"][i]["name"]){
+		if(layerName === config["layers"][i]["name"]){
 			for(var j=0; j<config["layers"][i]["fields"].length; j++)
 			{
 				layerKeys.set(config["layers"][i]["fields"][j]["fieldName"],config["layers"][i]["fields"][j]["fieldType"]);			
@@ -85,7 +85,7 @@ function listPropertiesByLayer(config){
 function createForm(map, fileFeatures, config, layer){
 	
 	const propKeys = listPropertiesByFeatures(fileFeatures);
-	const layerKeys = listPropertiesByLayer(config);
+	const layerKeys = listPropertiesByLayer(selectAdd.value, config);
 
 	const fileForm = document.createElement("form");
 	for (const [key, value] of layerKeys) {
